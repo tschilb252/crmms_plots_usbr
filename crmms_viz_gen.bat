8be71c19d0ae55490332ffd1bdb8b3f2f22e1d9f
Beau Uriona <buriona@usbr.gov>
2021-04-06 13:16:58 -0600

    stupid wizardry to get the mtom stuff to work

--- crmms_viz_gen.bat
+++ crmms_viz_gen.bat
@@ -7,6 +7,6 @@ echo hdb_env Activated!
 set wd=%~dp0
 call cd wd
 echo Starting CRMMS plots generation...
-python crmms_viz_gen.py --config %2 --output %3 --config_path %4 --no_mtom 1>%wd%/logs/run_all.out 2>%wd%/logs/run_all.err
+python crmms_viz_gen.py --config %2 --output %3 --config_path %4 1>%wd%/logs/run_all.out 2>%wd%/logs/run_all.err
 echo Process Complete!
 exit %errorlevel%
